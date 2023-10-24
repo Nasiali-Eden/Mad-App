d4281a752fd3a864041fa8c99d2d7c1ec1295aa6
nasiali-eden <nasieden0@gmail.com>
2023-10-24 10:18:16 +0300

    Modified

--- app/src/main/java/com/example/mainpage/RegistrationActivity.java
+++ app/src/main/java/com/example/mainpage/RegistrationActivity.java
@@ -16,18 +16,8 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.signup);
 
         EditText Password1 = findViewById(R.id.Password1);
-        EditText Password2 = findViewById(R.id.Password2);
 
-        CreateAccount = findViewById(R.id.CreateAccount);12e vb-jhgfds]451226=
-        CreateAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!Password1.getText().toString().equals(Password2.getText().toString())){
-                    Toast.makeText(RegistrationActivity.this, "Make sure both Passwords Match", Toast.LENGTH_SHORT).show();
-                }
 
-            }
-        });
+        };
     }
 
-}
